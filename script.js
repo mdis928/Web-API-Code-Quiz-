2e5857fcabac2e43ffeb28c184875f266c93b5c8
Michael DiSanto <mdisanto928@gmail.com>
2021-03-23 19:51:02 -0400

    second push

--- script.js
+++ script.js
@@ -1,24 +1,114 @@
 var startButton = document.getElementById("start-btn")
+var questionContainerElement = document.getElementById("question-container")
 
-startButton.addEventListener("click", startGame)
+var questionElement = document.getElementById("question")
+var answerButtonsElement = document.getElementById("answer-buttons")
 
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener("click", startGame) 
 
 
 
 // To start the game and timer begins
 function startGame() {
     console.log ("Started")
+    startButton.classList.add("hide")
+    shuffledQuestions = questions.sort(()=> Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove("hide")
+    setNextQuestion()
 }
 
 
 
 // setting our next question
 function setNextQuestion () {
-
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
+
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+
 
 
 // When selecting a question
-function selectAnswer () {
+function selectAnswer (e) {
+    const selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonElements.children).forEach(button => {setStatusClass(button, button.dataset.correct)})
 
 }
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element) 
+    if (correct) {element.classList.add("correct")
+    } else {element.classList.add("wrong")
+    }
+}
+
+
+
+
+var questions = [
+    {
+        question: "Which of the following is a disadvantage of using JavaScript?",
+        answers: [
+            {text: "Client-side JS does not allow the reading or writing of file", Correct: false},
+            {text: "JS cannot be used for Networking applications because there is no such support available", Correct: false},
+            {text: "JS doesn't have any mutltithreading or mutliprocess capabilties", Correct: false},
+            {text: "All of the above", Correct: true}
+        ] 
+    },
+    {
+        question: "Which of the following is true about cookie handling in JavaScript?",
+        answers: [
+            {text: "JavaScript can manipulate cookies using the cookie property of the Document object.", Correct: false},
+            {text: "JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.", Correct: false},
+            {text: "Both of the above.", Correct: true},
+            {text: "None of the above.", Correct: false},
+
+        ]
+    },
+    {
+        question: "Which built-in method returns the character at the specified index?",
+        answers: [
+            {text: "characterAt()", Correct: false},
+            {text: "getCharAt()", Correct: false},
+            {text: "charAt()", Correct: true},
+            {text: "None of the above.", Correct: false},
+        ]
+    },
+        {question: "Which built-in method reverses the order of the elements of an array?",
+        answers: [
+            {text: "changeOrder(order)", Correct: false},
+            {text: "reverse()", Correct: true},
+            {text: "sort(order)", Correct: false},
+            {text: "None of the above.", Correct: false}
+        ]
+    },
+    {
+       question: "Which of the following function of Number object returns a string value version of the current number?", 
+       answers: [
+           {text: "toString()", Correct: true},
+           {text: "toFixed()", Correct: false},
+           {text: "toLocaleString()", Correct: false},
+           {text: "toPrecision()", Correct: false},
+
+       ]
+    }
+]
