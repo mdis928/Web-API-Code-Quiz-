8211adefff668c65d1fd73c765b814e54d102bd4
Michael DiSanto <mdisanto928@gmail.com>
2021-03-23 21:22:41 -0400

    fourth push

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+var button = document.createElement('button')
 var startButton = document.getElementById("start-btn")
 var questionContainerElement = document.getElementById("question-container")
-
 var questionElement = document.getElementById("question")
 var answerButtonsElement = document.getElementById("answer-buttons")
 
@@ -32,7 +32,7 @@ function setNextQuestion () {
 function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
-        var button = document.createElement('button')
+        
         button.innerText = answer.text
         button.classList.add('btn')
         if (answer.correct) {
