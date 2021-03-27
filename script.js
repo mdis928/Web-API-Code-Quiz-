8af46743c2ed87307e9f694f19037645f3a8b206
Michael DiSanto <mdisanto928@gmail.com>
2021-03-27 10:11:00 -0400

    sixth update

--- script.js
+++ script.js
@@ -3,22 +3,23 @@ var startButton = document.getElementById("start-btn")
 var questionContainerElement = document.getElementById("question-container")
 var questionElement = document.getElementById("question")
 var answerButtonsElement = document.getElementById("answer-buttons")
-var Countdown = 75
+var Countdown = 76
 
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame) 
 
-
+var timerID
 
 // To start the game and timer begins
 function startGame() {
-    var timerID = setInterval (function(){
+    timerID = setInterval (function(){
         Countdown--;
         document.getElementById ("timer").textContent= Countdown;
-        if (Countdown === 0){
+        if (Countdown <= 0){
             clearInterval(timerID); 
-        document.getElementById('timer').textContent="You are out of Time!"       
+        document.getElementById('timer').textContent="You are out of Time!"   
+        QuizOver()    
         }
     }, 1000)
     console.log ("Started")
@@ -33,22 +34,31 @@ function startGame() {
 function setNextQuestion () {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
-    
 }
 
 
 function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        var button = document.createElement('button')    
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
+    var currentQ = questions [currentQuestionIndex]
+    questionElement.innerText = currentQ.question
+    answerButtonsElement.innerHTML= ""
+    currentQ.answers.forEach(function(choice,i){
+        var choiceBtn = document.createElement("button")
+        choiceBtn.setAttribute("class",'choice')
+        choiceBtn.setAttribute("value",choice)
+        choiceBtn.textContent=i+1+" "+choice
+        choiceBtn.onclick = selectAnswer
+        answerButtonsElement.appendChild(choiceBtn)
     })
+    // question.answers.forEach(answer => {
+    //     var button = document.createElement('button')    
+    //     button.innerText = answer.text
+    //     button.classList.add('btn')
+    //     if (answer.correct) {
+    //         button.dataset.correct = answer.correct
+    //     }
+    //     button.addEventListener('click', selectAnswer)
+    //     answerButtonsElement.appendChild(button)
+    // })
 }
 
 function resetState() {
@@ -61,25 +71,29 @@ function resetState() {
 
 // When selecting a question
 function selectAnswer (e) {
-    const selectedButton = e.target
-    var correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonElements.children).forEach(button => {setStatusClass(button, button.dataset.correct)})
-}
-
-function setStatusClass(element,correct) {
-    clearStatusClass(element)
-    if (correct) {
-      element.classList.add("correct")  
-    } else {
-        element.classlist.add("wrong")
+    // const selectedButton = e.target
+    // var correct = selectedButton.dataset.Correct
+    // setStatusClass(document.body, correct)
+    // Array.from(answerButtonsElement.children).forEach(button => {setStatusClass(button, button.dataset.Correct)})
+    if(this.value !== questions[currentQuestionIndex].correct){
+        Countdown -=15
+    }
+    currentQuestionIndex++ 
+    console.log(currentQuestionIndex)
+    console.log(questions.length)
+    if(currentQuestionIndex === questions.length){
+        clearInterval(timerID)
+        QuizOver() 
+    }
+    else{
+        showQuestion()
     }
 }
 
-function clearStatusClass(element) {
-    element.classList.remove("correct")
-    element.classList.remove("wrong")
+function QuizOver(){
+
 }
+
 
 
 
@@ -88,47 +102,51 @@ var questions = [
     {
         question: "Which of the following is a disadvantage of using JavaScript?",
         answers: [
-            {text: "Client-side JS does not allow the reading or writing of file", Correct: false},
-            {text: "JS cannot be used for Networking applications because there is no such support available", Correct: false},
-            {text: "JS doesn't have any mutltithreading or mutliprocess capabilties", Correct: false},
-            {text: "All of the above", Correct: true}
-        ] 
+            "Client-side JS does not allow the reading or writing of file", 
+             "JS cannot be used for Networking applications because there is no such support available", 
+             "JS doesn't have any mutltithreading or mutliprocess capabilties", 
+             "All of the above", 
+        ], 
+        correct: "All of the above"
     },
     {
         question: "Which of the following is true about cookie handling in JavaScript?",
         answers: [
-            {text: "JavaScript can manipulate cookies using the cookie property of the Document object.", Correct: false},
-            {text: "JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.", Correct: false},
-            {text: "Both of the above.", Correct: true},
-            {text: "None of the above.", Correct: false},
-
-        ]
+            "JavaScript can manipulate cookies using the cookie property of the Document object.", 
+            "JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.", 
+             "Both of the above.",
+             "None of the above.",
+        ],
+        correct: "Both of the above"
     },
     {
         question: "Which built-in method returns the character at the specified index?",
         answers: [
-            {text: "characterAt()", Correct: false},
-            {text: "getCharAt()", Correct: false},
-            {text: "charAt()", Correct: true},
-            {text: "None of the above.", Correct: false},
-        ]
+             "characterAt()", 
+            "getCharAt()", 
+             "charAt()", 
+            "None of the above.", 
+        ],
+        correct: "CharAt()"
     },
         {question: "Which built-in method reverses the order of the elements of an array?",
         answers: [
-            {text: "changeOrder(order)", Correct: false},
-            {text: "reverse()", Correct: true},
-            {text: "sort(order)", Correct: false},
-            {text: "None of the above.", Correct: false}
-        ]
+            "changeOrder(order)", 
+            "reverse()", 
+            "sort(order)", 
+             "None of the above.",
+        ],
+        correct: "reverse()"
     },
     {
        question: "Which of the following function of Number object returns a string value version of the current number?", 
        answers: [
-           {text: "toString()", Correct: true},
-           {text: "toFixed()", Correct: false},
-           {text: "toLocaleString()", Correct: false},
-           {text: "toPrecision()", Correct: false},
+           "toString()", 
+            "toFixed()", 
+           "toLocaleString()", 
+            "toPrecision()", 
 
-       ]
+       ],
+       correct: "toString()"
     }
 ]
